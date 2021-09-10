5689a0c2dc23be78da386cb20c14d18f3869c39f
David A <davidaster007@gmail.com>
2021-09-10 13:57:11 +0200

    added SharedPrefs project. Added SharedPreferences implementation, that allow us saving a data.

---
+++ app/src/main/java/com/example/sprefs/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.sprefs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String MESSAGE_ID = "messages_prefs";
+    private Button saveButton;
+    private EditText enterMessage;
+    private TextView showMessageTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        saveButton = findViewById(R.id.button_save_message);
+        enterMessage = findViewById(R.id.editText_enter_message);
+        showMessageTextView = findViewById(R.id.show_message_textView);
+
+        saveButton.setOnClickListener(view -> {
+            String message = enterMessage.getText().toString().trim();
+
+            SharedPreferences sharedPreferences = getSharedPreferences(MESSAGE_ID, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("message", message);
+
+            editor.apply();
+        });
+
+
+
+    }
+}
